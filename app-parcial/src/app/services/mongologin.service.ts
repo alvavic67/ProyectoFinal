a8a6f9e9fc88ec5a925bec9cdb857d8fdff9921f
Alvaro Silva <alvavic67@hotmail.com>
2020-06-22 17:00:01 -0500

    add NodeJs ip

--- app-parcial/src/app/services/mongologin.service.ts
+++ app-parcial/src/app/services/mongologin.service.ts
@@ -10,7 +10,7 @@ const urlAPI = environment.API.EndPoint.NodeJS;
 })
 export class MongologinService {
 
-  constructor(private http:HttpClient) { 
+  constructor(private http: HttpClient) {
 
   }
 
--- app-parcial/src/environments/environment.prod.ts
+++ app-parcial/src/environments/environment.prod.ts
@@ -1,8 +1,9 @@
 export const environment = {
   production: true,
   API: {
-    EndPoint:{
-      Northwind: 'http://api.northwind.com/API.Northwind/v1/Analysis/Northwind/'
+    EndPoint: {
+      Northwind: 'http://api.northwind.com/API.Northwind/v1/Analysis/Northwind/',
+      NodeJS: 'http://192.168.10.100:3000/api/'
     }
   }
 };
